4c8e4b74df80f8e097040a0777a5ef1439bde7b4
Maria Sorokina <ruafelianna@ya.ru>
2025-04-22 16:40:54 +0300

    Add script to run auto-install archlinux on dev vm through ssh

---
+++ archlinux/vm-dev-vbox/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+source "$SCRIPT_DIR/env.sh"
+
+yes | scp.exe -P $VM_PORT -r ../../../linux-utils/ root@$VM_HOST:/root
+yes | ssh -p $VM_PORT root@$VM_HOST "cd linux-utils && ./archlinux/vm-dev/install.sh"
---
+++ archlinux/vm-dev-vbox/sample.env.sh
@@ -0,0 +1,12 @@
+VM_HOST=
+VM_PORT=
+
+if [ -z "$VM_PORT" ]; then
+    log "Error: VM_PORT var is not defined in env.sh"
+    exit 1
+fi
+
+if [ -z "$VM_PORT" ]; then
+    log "Error: VM_PORT var is not defined in env.sh"
+    exit 1
+fi
