1fa886cee89fc8b6ba9e04b2e2a85f79e2b5ae67
Maria Sorokina <ruafelianna@ya.ru>
2025-05-30 09:59:58 +0300

    Fix typo

--- archlinux/vm-dev-vbox/install.sh
+++ archlinux/vm-dev-vbox/install.sh
@@ -5,5 +5,5 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 source "$SCRIPT_DIR/env.sh"
 
-yes | scp.exe -P $VM_PORT -r ../../../linux-utils/ root@$VM_HOST:/root
+yes | scp -P $VM_PORT -r ../../../linux-utils/ root@$VM_HOST:/root
 yes | ssh -p $VM_PORT root@$VM_HOST "cd linux-utils && ./archlinux/vm-dev/install.sh"
